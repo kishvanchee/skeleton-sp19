c476800d5a3cd5963e697f755db4d75edfc728b5
Kishore Vancheeshwaran <kishvanchee@yahoo.co.in>
2020-04-12 17:56:21 +0530

    private

--- proj0/Body.java
+++ proj0/Body.java
@@ -5,7 +5,7 @@ public class Body {
     public double yyVel;
     public double mass;
     public String imgFileName;
-    public static final double g = 6.67E-11;
+    private static final double g = 6.67E-11;
 
 
     public Body(double xP, double yP, double xV, double yV, double m, String img) {
